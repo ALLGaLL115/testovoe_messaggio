1ccafc9710e675b87798808c67d216bf95928418
Ali <all.gall1@mail.ru>
2024-07-29 21:58:39 +0300

    Add message CRUD

---
+++ internal/storage/message/message.go
@@ -0,0 +1,132 @@
+package message
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log/slog"
+
+	"github.com/ALLGaLL115/testovoe-messaggio/internal/domain/dto"
+	"github.com/ALLGaLL115/testovoe-messaggio/internal/domain/models"
+	"github.com/ALLGaLL115/testovoe-messaggio/lib/logger/sl"
+	"github.com/ALLGaLL115/testovoe-messaggio/lib/storage/query"
+	"github.com/jackc/pgx/v5"
+)
+
+type MessageDB struct {
+	log *slog.Logger
+}
+
+func newMessageDB(log *slog.Logger) *MessageDB {
+	return &MessageDB{
+		log: log,
+	}
+}
+
+const (
+	messageTable = "messages"
+)
+
+var (
+	ErrMessageNotFound  = errors.New("message not found")
+	ErrMessagesNotFound = errors.New("messages not found")
+)
+
+func (m *MessageDB) CreateMessage(ctx context.Context, tx pgx.Tx, message dto.Message) (int64, error) {
+	const op = "storage.message.CreateMessage"
+
+	q := fmt.Sprintf(`
+	INSERT INTO %s
+		(chat_id, text, created_at)
+	VALUES 
+		($1, $2, $3, $4)
+			RETURNING id;
+	`, messageTable)
+
+	m.log.Debug("create message query", slog.String("query", query.QueryToString(q)))
+
+	var messageID int64
+
+	err := tx.QueryRow(ctx, q, message.ID, message.Text, message.CreatedAt).Scan(&messageID)
+
+	if err != nil {
+		m.log.Error("failed to create message", sl.OpError(op, err))
+		return 0, err
+	}
+
+	return messageID, nil
+}
+
+func (m *MessageDB) GetMessageById(ctx context.Context, tx pgx.Tx, messageID int64) (models.Message, error) {
+	const op = "storage.message.CreateMessage"
+
+	q := fmt.Sprintf(`
+	SELECT 
+		id, text, created_at
+	FROM %s
+	WHERE id = $1
+	`, messageTable)
+
+	m.log.Debug("get message by id query:", slog.String("query", query.QueryToString(q)))
+
+	var message models.Message
+	err := tx.QueryRow(ctx, q, messageID).Scan(&message.ID, &message.Text, &message.CreatedAt)
+
+	if err != nil {
+		if err == pgx.ErrNoRows {
+			return models.Message{}, ErrMessageNotFound
+		}
+		m.log.Error("failed to get message by id", sl.OpError(op, err))
+		return models.Message{}, err
+	}
+
+	return message, nil
+
+}
+
+func (m *MessageDB) UpdateMessageByID(ctx context.Context, tx pgx.Tx, message dto.Message) (int64, error) {
+	const op = "storage.message.UpdateMessageByID"
+
+	q := fmt.Sprintf(`
+	UPDATE %s
+	SET  text=$2
+	WHERE id = $1
+	RETURNING id
+
+	`, messageTable)
+
+	m.log.Debug("update message by id: ", slog.String("query", query.QueryToString(q)))
+
+	var messageID int64
+	err := tx.QueryRow(ctx, q, message.ID, message.Text).Scan(&messageID)
+
+	if err != nil {
+		m.log.Error("failed to update a message: ", sl.OpError(op, err))
+		return 0, err
+	}
+
+	return messageID, nil
+}
+
+func (m *MessageDB) DeleteMessageByID(ctx context.Context, tx pgx.Tx, messageID int64) (int64, error) {
+	const op = "storage.message.DeleteMessageByID"
+
+	q := fmt.Sprintf(`
+	DELETE FROM %s
+	WHERE id = $1
+	RETURNING id
+	`, messageTable)
+
+	m.log.Debug("delete message by id: ", slog.String("query", query.QueryToString(q)))
+
+	var messageId int64
+	err := tx.QueryRow(ctx, q, messageID).Scan(&messageId)
+
+	if err != nil {
+		m.log.Error("failed to delete message: ", sl.OpError(op, err))
+		return 0, err
+	}
+
+	return messageId, nil
+
+}
--- internal/storage/postgresql/postgresql.go
+++ internal/storage/postgresql/postgresql.go
@@ -4,8 +4,13 @@ import (
 	"context"
 	"fmt"
 	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/ALLGaLL115/testovoe-messaggio/internal/config"
+	"github.com/ALLGaLL115/testovoe-messaggio/lib/logger/sl"
+	"github.com/ALLGaLL115/testovoe-messaggio/lib/storage/repetable"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 )
@@ -24,4 +29,36 @@ func NewConnection(ctx context.Context, log *slog.Logger, cfg config.DataBase) (
 
 	var pool *pgxpool.Pool
 
+	err := repetable.DoWithTries(func() error {
+		log.Info("Database connection attemp")
+		ctx, cancel := context.WithTimeout(ctx, cfg.Timeout)
+
+		defer cancel()
+
+		pool, _ = pgxpool.New(ctx, dsn)
+		err := pool.Ping(ctx)
+
+		if err != nil {
+			log.Error("database connection error")
+		}
+
+		return err
+	}, cfg.Attempts, cfg.Delay)
+
+	if err != nil {
+		log.Error("falied connect to database", sl.OpError(op, err))
+		return nil, err
+	}
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGTERM, syscall.SIGINT)
+
+	go func() {
+		stopSignal := <-stop
+		log.Info("stoppping database connection", slog.String("op", op), slog.String("signal", stopSignal.String()))
+		pool.Close()
+		log.Info("database was stopped")
+	}()
+
+	return pool, nil
 }
